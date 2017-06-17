e1d7dc2cf6418da8798ec10f417eb39f9949f604
Mauricio Manoel <mauriciomanoel@gmail.com>
2017-06-17 15:42:17 -0300

    add ao projeto o fonte arduino

---
+++ DHT11/DHT11.ino
@@ -0,0 +1,47 @@
+#include "DHT.h"
+ 
+#define DHTPIN A1 // Pino que estamos conectado
+#define DHTTYPE DHT11 // Modelo do Sensor
+ 
+DHT dht(DHTPIN, DHTTYPE);
+
+String comando, temperatura, umidade;
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+  delay(1000); //Delay para o início da execução do programa
+}
+ 
+void loop() 
+{
+  // Carregando os valores 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Falha ao ler o sensor ");
+    
+    comando = "python /home/pi/create_html_dht11.py nok"; // String com o comando para execução no system
+    Serial.println(comando);
+    delay(2000); //Delay de dois segundos entre as leituras da informação
+  } 
+  else
+  {
+    temperatura = String(t);
+    umidade = String(h);
+    
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.print("*C ");
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.println("%  ");
+    
+    comando = "python /home/pi/create_html_dht11.py ok " + temperatura + " " + umidade; // String com o comando para execução no system
+    Serial.println(comando);
+    
+    delay(2000); //Delay de dois segundos entre as leituras da informação
+  }
+}
