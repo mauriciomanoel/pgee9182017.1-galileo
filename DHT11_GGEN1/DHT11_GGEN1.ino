833b4871d84a154e950888d75b696667cc213565
Mauricio Manoel <mauriciomanoel@gmail.com>
2017-06-21 15:00:02 -0300

    update do projeto para Galileo Gen 1

---
+++ DHT11_GGEN1/DHT11_GGEN1.ino
@@ -0,0 +1,54 @@
+#include "DHT.h"
+ 
+#define DHTIN A1     // what pin we're connected to
+#define DHTOUT 3
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht(DHTIN,DHTOUT, DHTTYPE);
+
+String comando, temperatura, umidade;
+const char* cmd;
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+  delay(1000); //Delay para o início da execução do programa
+}
+ 
+void loop() 
+{
+  // Carregando os valores 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  /*if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Falha ao ler o sensor ");
+    
+    comando = "python /home/pi/create_html_dht11.py nok 0 0 > /dev/ttyGS0"; // String com o comando para execução no system
+    //cmd = comando.c_str(); // convertendo os dados para contante char;
+    //system(comando);
+    //Serial.println(comando);
+    delay(2000); //Delay de dois segundos entre as leituras da informação
+  } 
+  else
+  {*/
+    //temperatura = String(t);
+    //umidade = String(h);
+    
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.print("*C ");
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.println("%  ");
+    
+    //comando = "python /home/pi/create_html_dht11.py ok " + temperatura + " " + umidade + " > /dev/ttyGS0"; // String com o comando para execução no system    
+    //cmd = comando.c_str(); // convertendo os dados para contante char;
+    //system(comando);
+    //Serial.println(cmd);
+    delay(2000); //Delay de dois segundos entre as leituras da informação
+  //}
+}
